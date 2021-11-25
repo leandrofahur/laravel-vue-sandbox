9cbc04abf2f4366ba27cd34e90aaff0ada028051
Leandro Miranda Fahur Machado <leandro@Leandros-MacBook-Pro-2.local>
2021-11-24 20:32:21 -0800

    a few CRUD with postman

--- todolist/app/Http/Controllers/ItemController.php
+++ todolist/app/Http/Controllers/ItemController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Carbon;
 class ItemController extends Controller
 {
     /**
@@ -36,7 +36,7 @@ class ItemController extends Controller
     public function store(Request $request)
     {
         $newItem = new Item;
-        $newItem->name = $request->item['name'];
+        $newItem->name = $request->item["name"];
         $newItem->save();
 
         return $newItem;
@@ -73,7 +73,17 @@ class ItemController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $existingItem = Item::find($id);
+        if($existingItem) {
+            $existingItem->completed = $request->item["completed"] ? true : false;
+
+            $existingItem->completed_at = $request->item["completed"] ? Carbon::now() : null;
+
+            $existingItem->save();
+            return $existingItem;
+        }
+
+        return "Item not found!";
     }
 
     /**
--- todolist/routes/web.php
+++ todolist/routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ItemController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,12 +15,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-// Item route:
-Route::get('/items', [ItemController::class, 'index']);
-Route::prefix('/item')->group(function () {
-    Route::post('/store', [ItemController::class,'store']);
-    Route::put('/{id}', [ItemController::class,'update']);
-    Route::delete('/{id}', [ItemController::class,'destroy']);
 });
